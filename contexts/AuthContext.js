6fa930a465d120e420041ed5a8c1967bcd1879da
gustavoabell <gustavoabel.contato@gmail.com>
2021-05-20 13:56:46 -0300

    test commit

---
+++ contexts/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState } from 'react';
+import Router from 'next/router';
+import firebase from '../lib/firebase';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    const signin = () => {
+        try {
+        setLoading(true);
+        return firebase
+        .auth()
+        .sigInWithPopup(new firebase.auth.GithubAuthProvider())
+        .then((response) => {
+            setUser(response.user);
+            Router.push('/dashboard');
+        });
+    } finally {
+        setLoading(false);
+    }
+    
+}
+
+    const signout = () => {
+        try{
+            Router.push('/');
+
+            return firebase
+            .auth()
+            .signOut()
+            .then(() => setUser(false));
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return <AuthContext.Provider value={{
+        user,
+        loading,
+        signin,
+        signout,
+    }}>{children}</AuthContext.Provider>
+}
+
+    
+
+export const AuthConsumer = AuthContext.Consumer;
+
+export default AuthContext;
---
+++ pages/dashboard.js
@@ -0,0 +1,6 @@
+function Dashboard() {
+    return (<h1>Dash</h1>)
+
+}
+
+export default Dashboard;
